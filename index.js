95d621098016f5ce471be182dd83a1df77ccfa40
ybmin <nimby@sparcs.org>
2024-02-06 18:23:54 +0900

    Fix: Change wrong query selection

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 
 const mongoose = require("mongoose");
-const { validationResult } = require("express-validator");
+const { validationResult, check } = require("express-validator");
 
-const initDatabase = require("./init");
 const locations = require("./src/static/locations.json");
 const TaxiFare = require("./src/store/mongo.ts");
 
 // load env
 dotenv.config();
 
+// Naver Cloud Platform Maps Directions 5 API Keys
 const NAVER_MAP_API_KEYS = {
   "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_MAP_API_ID,
   "X-NCP-APIGW-API-KEY": process.env.NAVER_MAP_API_KEY,
@@ -21,11 +21,40 @@ const NAVER_MAP_API_KEYS = {
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "Test Taxi Fare" });
+  res.send("Taxi Fare API");
 });
 
 // Initialize database
 // Erase all previous data and sets all taxi fare to 0
+async function initDatabase() {
+  await mongoose.connect("mongodb://localhost:27017/taxiFareDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Remove all previous data
+  await TaxiFare.deleteMany({});
+
+  for (let skey in locations) {
+    for (let gkey in locations) {
+      if (skey === gkey) continue;
+
+      let tableFare = [];
+      for (let i = 0; i < 48; i++) {
+        tableFare.push({
+          start: skey,
+          goal: gkey,
+          time: i,
+          fare: 0,
+        });
+      }
+
+      await TaxiFare.insertMany(tableFare);
+    }
+  }
+
+  console.log("TaxiFare Database initialized");
+}
 app.get("/taxiFare/init", async (req, res) => {
   try {
     await initDatabase();
@@ -37,18 +66,30 @@ app.get("/taxiFare/init", async (req, res) => {
   }
 });
 
-// Main
-app.get("/taxiFare/:start/:goal/:time", async (req, res) => {
+const getTaxiFare = async (req, res) => {
   try {
     let start = locations[req.params.start];
     let goal = locations[req.params.goal];
     let time = new Date(req.params.time);
     let sTime = time.getHours() * 2 + Math.floor(time.getMinutes() / 30); // Scaled Time. 0 ~ 47 (0:00 ~ 23:30)
 
-    let taxiFare = await TaxiFare.findOne({ start, goal, time: sTime });
+    let taxiFare = await TaxiFare.findOne(
+      {
+        start: req.params.start,
+        goal: req.params.goal,
+        time: sTime,
+      },
+      function (err, res) {
+        if (err) console.log(err);
+      }
+    ).clone();
 
-    if (taxiFare && new Date() - taxiFare.updatedAt < 24 * 60 * 60 * 1000) {
-      res.send({ fare: taxiFare.fare });
+    if (
+      taxiFare &&
+      new Date() - taxiFare.updatedAt < 24 * 60 * 60 * 1000 &&
+      taxiFare.fare !== false
+    ) {
+      res.json({ fare: taxiFare.fare });
       return;
     } else {
       let response = await axios.get(
@@ -58,21 +99,36 @@ app.get("/taxiFare/:start/:goal/:time", async (req, res) => {
 
       let fare = response.data.route.traoptimal[0].summary.taxiFare;
       if (!taxiFare) {
-        taxiFare = new TaxiFare({ start, goal, time: sTime, fare: fare }); // 진짜 만일에 document가 공백이 생겼을 경우 채우는 용도
+        taxiFare = new TaxiFare(
+          {
+            start: req.params.start,
+            goal: req.params.goal,
+            time: sTime,
+            fare: fare,
+          },
+          function (err, docs) {
+            if (err) console.log(err);
+            else console.log("TaxiFare Document Created: " + docs);
+          }
+        ); // 진짜 만일 document가 중간에 삭제되어 공백이 생겼을 경우 채우는 용도
       }
 
       await TaxiFare.updateOne(
-        { start: start, goal: goal, time: sTime },
-        { fare: fare }
-      );
-
-      res.json({ fare });
+        { start: req.params.start, goal: req.params.goal, time: sTime },
+        { fare: fare },
+        function (err, docs) {
+          if (err) console.log(err);
+          else console.log("Document Updated: " + docs);
+        }
+      ).clone();
+      res.json({ fare: fare });
+      return;
     }
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Failed with exception " + err.message });
   }
-});
+};
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
@@ -96,6 +152,9 @@ const checkTaxiFareParams = [
     .withMessage("날짜/시간 형식이 올바르지 않습니다"),
   validate,
 ];
+
+// Get taxi fare
+app.get("/taxiFare/:start-:goal/time/:time", checkTaxiFareParams, getTaxiFare);
 
 mongoose
   .connect("mongodb://localhost:27017/taxiFareDB", {
--- src/store/mongo.ts
+++ src/store/mongo.ts
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
-// Define Schemes
 const taxiFareSchema = new mongoose.Schema(
   {
-    start: { type: String, required: true },
-    goal: { type: String, required: true },
-    time: { type: Number, required: true },
-    fare: { type: Number, default: false },
+    start: { type: String, required: true }, // 출발지
+    goal: { type: String, required: true }, // 도착지
+    time: { type: Number, required: true }, // 출발 시간 (24h를 30분 단위로 분리, 0 ~ 47 (0:00 ~ 23:30))
+    fare: { type: Number, default: false }, // 예상 택시 요금
   },
   {
-    timestamps: true,
+    timestamps: true, // 최근 업데이트 시간 기록용
   }
 );
 // Create Model & Export
